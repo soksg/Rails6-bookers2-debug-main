eafccb24a25eb1efad678687226645a527b88dc6
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-25 07:58:38 +0000

    [Fix]応用課題2バリデーション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
-
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
