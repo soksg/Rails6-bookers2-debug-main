caba653b4b90a2ac0ff5bc6697f0333f0e0b509d
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-27 12:54:46 +0000

    [Fix]bootstrap,users/index修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user=current_user
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.book
+    @books = @user.books
     @book = Book.new
   end
 
