ade02783e4a06cb5a855fc68cfbfa4f323ba739c
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-28 06:42:50 +0000

    [Fix]応用課題2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
 
   def edit
     @user=User.find(params[:id])
+    # unless @user=current_user
+    #   redirect_to user_path(current_user.id)
+    # end
   end
 
   def update
