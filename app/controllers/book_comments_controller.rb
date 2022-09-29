baf9e8caa3df944ebc0d5704a09465f00bd5cb64
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-29 08:21:57 +0000

    [Add]応用課題３　コメント入力フォーム

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,7 @@
 class BookCommentsController < ApplicationController
   def create
     book=Book.find(params[:book_id])
-    commnet=current_user.book_comments.new(book_comment_params)
+    comment=current_user.book_comments.new(book_comment_params)
     comment.book_id=book.id
     comment.save
     redirect_to book_path(book)
