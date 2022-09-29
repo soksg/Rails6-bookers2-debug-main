cce57f9a3292ec164796556543715783a078c749
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-29 01:36:08 +0000

    [Add]応用課題３/favorites_contoroller

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
 class Favorite < ApplicationRecord
-  belongs_to :user,  dependent: :destroy
-  belongs_to :book,  dependent: :destroy
+  belongs_to :user
+  belongs_to :book
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
 
   has_many :books, dependent: :destroy
-  has_many :favorite
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
